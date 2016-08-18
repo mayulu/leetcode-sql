351fe091756a9df377244c2b5b02e0c5f713bc82
mayulu <mayulu@gmail.com>
2016-08-18 16:43:24 +0800

    175-Finished.175-Finished.175-Finished.175-Finished.175-Finished.175-Finished.175-Finished.

--- 175-Combine-Two-Tables.sql
+++ 175-Combine-Two-Tables.sql
@@ -1,1 +1,11 @@
+## 1. Left Join
 
+select FirstName, LastName, City, State
+from Person p left join Address a
+on p.PersonId = a.PersonId
+
+
+## 2. Traditional method
+select FirstName, LastName, City, State
+from Person p, Address a
+where p.PersonId = a.PersonId 
