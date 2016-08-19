1108256432b969af99d1a359e80c545709a11741
mayulu <mayulu@gmail.com>
2016-08-19 09:58:43 +0800

    Done-176-2

--- 176-Second-Highest-Salary.sql
+++ 176-Second-Highest-Salary.sql
@@ -1,1 +1,10 @@
+# 1. Distinct limit
+select distinct salary as SecongHighSalary
+from Employee 
+order by salary desc 
+limit 1, 1;
 
+#2. Subquery
+select max(salary)
+from Employee
+where salary < (select max(salary) from Employee);
